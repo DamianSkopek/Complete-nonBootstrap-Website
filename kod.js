5cb321336227efc273a19d8700fe97589aace4de
DamianSkopek <skopekdamian@gmail.com>
2020-10-28 11:24:52 +0100

    3-col-section

--- kod.js
+++ kod.js
@@ -8,17 +8,22 @@ const toggleNavButton = () =>{
     navButtonFont.classList.toggle('toggle-nav-btn')
 }
 navButton.addEventListener('click', toggleNavButton)
+navButton.addEventListener('blur', ()=>{
+    navLinks.classList.remove('toggle-nav-container-btn')
+    navButtonFont.classList.remove('toggle-nav-btn')
+})
 
 
 
 // ======== Carousel Images ========
 const caselSlide = document.querySelector('.casel-slide')
-const caselImages = document.querySelectorAll('.casel-slide img')
+const caselImages = document.querySelectorAll('.casel-img-container')
 const prevButton = document.querySelector('.prev-btn')
 const nextButton = document.querySelector('.next-btn')
 
 let counter = 1
-const caselSize = caselImages[0].clientWidth
+let caselSize = caselImages[0].clientWidth
+
 caselSlide.style.transition = "none"
 caselSlide.style.transform = `translateX(${-caselSize*counter}px)`
 
@@ -39,17 +44,35 @@ const caselPrev = () =>{
 prevButton.addEventListener('click', caselPrev)
 
 const caselLoop = () =>{
-    if(caselImages[counter].className === "last-clone"){
+    if(caselImages[counter].className.search("last-clone")!==-1){
         caselSlide.style.transition = "none"
         counter = caselImages.length - 2
         caselSlide.style.transform = `translateX(${-caselSize*counter}px)`
     }
-    if(caselImages[counter].className === "first-clone"){
+    if(caselImages[counter].className.search("first-clone")!==-1){
         caselSlide.style.transition = "none"
         counter = caselImages.length - 4
         caselSlide.style.transform = `translateX(${-caselSize*counter}px)`
     }
 }
 caselSlide.addEventListener('transitionend', caselLoop)
+window.addEventListener('resize', ()=>{
+    caselSize = caselImages[0].clientWidth
+    caselSlide.style.transform = `translateX(${-caselSize*counter}px)`
+})
+setInterval(caselNext, 990000)
 
-setInterval(caselNext, 10000)
+
+
+
+
+
+
+
+
+
+// zaraz usun
+const zaraUsun = document.querySelector('.zaraz-usun')
+window.addEventListener('click', ()=>{
+    zaraUsun.textContent = counter
+})
